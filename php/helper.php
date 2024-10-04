023fcec090db37fb5694089cd68cb15b5a666c8b
Kévin Tessier <kevin-tessier@protonmail.com>
2024-10-04 15:51:59 +0200

    add function pagniation

--- php/helper.php
+++ php/helper.php
@@ -28,6 +28,73 @@ class Helper
         return $result;
     }
 
-}
+    // PAGINATION
+    /**
+     * Undocumented function
+     *
+     * @param [type] $limit
+     * @return void
+     * @param
+     * @return
+     * @version 1.0.0
+     * @see url context
+     * @author Kevin Tessier <kevin-tessier@protonmail.com>
+     */
+    function findStart($limit) { 
+        if ((!isset($_GET['page'])) || ($_GET['page'] == "1")) { 
+            $start = 0; 
+            $_GET['page'] = 1; 
+        } else { 
+            $start = ($_GET['page']-1) * $limit; 
+        } 
+        return $start; 
+    }
+    /**
+     * Undocumented function
+     *
+     * @param [type] $count
+     * @param [type] $limit
+     * @return void
+     * @param
+     * @return
+     * @version 1.0.0
+     * @see url context
+     * @author Kevin Tessier <kevin-tessier@protonmail.com>
+     */
+    function findPages($count, $limit) { 
+        $pages = (($count % $limit) == 0) ? $count / $limit : floor($count / $limit) + 1; 
+        return $pages; 
+    } 
+    
+    /**
+     * Undocumented function
+     *
+     * @param [type] $curpage
+     * @param [type] $pages
+     * @return void
+     * @param
+     * @return
+     * @version 1.0.0
+     * @see url context
+     * @author Kevin Tessier <kevin-tessier@protonmail.com>
+     */
+    function nextPrev($curpage, $pages) { 
+        $next_prev  = ""; 
 
+        if (($curpage-1) <= 0) { 
+            $next_prev .= "Previous"; 
+        } else { 
+            $next_prev .= "<a href=\"".$_SERVER['PHP_SELF']."?page=".($curpage-1)."\">Previous</a>"; 
+        } 
+
+            $next_prev .= " | "; 
+
+        if (($curpage+1) > $pages) { 
+            $next_prev .= "Next"; 
+        } else { 
+            $next_prev .= "<a href=\"".$_SERVER['PHP_SELF']."?page=".($curpage+1)."\">Next</a>"; 
+        } 
+        return $next_prev; 
+    }
+}                           
 ?>
